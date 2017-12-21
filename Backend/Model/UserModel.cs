1591e17b5f22e0a1bb28061391ed709772b39e8d
guneet gee singh <guneetgeesingh@outlook.com>
2017-12-21 11:28:41 -0800

    User Controller Fix

--- Backend/Model/UserModel.cs
+++ Backend/Model/UserModel.cs
@@ -9,7 +9,9 @@ namespace iClinical.Model
   public class User
   {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public int Age { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
     public string Gender { get; set; }
