3885eb4e4b26a3a9d161933c2db9d6abb713193e
andre khuu <khuu.andre@gmail.com>
2017-12-22 10:09:10 -0800

    changes

---
+++ Frontend/Controllers/companyController.js
@@ -0,0 +1,5 @@
+app.controller("companyController", function ($scope, $state, $stateParams, userService) {
+
+//populates studies to dummy companies id:1,2,3
+
+    })
---
+++ Frontend/Services/companyService.js
@@ -0,0 +1,8 @@
+app.service("companyService", function ($http) {
+
+  //populates studies to dummy companies id:1,2,3
+  this.setupCompanies(id, data){
+    
+  }
+})
+  
