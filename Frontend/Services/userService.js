0bb2973203c4ae5f153e05dab4f8ba117571b5d2
andre khuu <khuu.andre@gmail.com>
2017-12-21 16:09:29 -0800

    signup to post to backend complete!!

--- Frontend/Services/userService.js
+++ Frontend/Services/userService.js
@@ -1,14 +1,13 @@
 app.service("userService", function ($http) {
 
   this.newUserForm = function (x) {
-    return $http.post("http://localhost:5000/api/user", x)
+    return $http.post("http://localhost:5000/api/users", x);
   }
 
   this.getUserById = function (id, callback) {
     if (id == "" || id == undefined || id == null) {
       //create empty user object pass user object to callback
       var user = {
-        id: 5,
         firstName: "",
         lastName: "",
         age: "",
@@ -35,7 +34,7 @@ app.service("userService", function ($http) {
         skin: false,
         stroke: false,
         generic: false,
-        other: false,
+        other: "none",
         obese: false,
         sendentary: false,
         diet: false,
