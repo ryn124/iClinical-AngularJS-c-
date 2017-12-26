5ab09eb4f8190320836d2e76b61200f30b3c01ae
ryan <ryfield12@gmail.com>
2017-12-26 14:49:31 -0800

    logout now logs a signed in company profile

--- Frontend/Controllers/navBarController.js
+++ Frontend/Controllers/navBarController.js
@@ -1,7 +1,8 @@
-app.controller("navBarController", function ($scope, $state, $stateParams, userService) {
+app.controller("navBarController", function ($scope, $state, $stateParams, userService, companyService) {
   // logout function
   $scope.logout = function(){
     userService.setCurrentUser(0);
+    companyService.setCurrentCompany(0);
     $state.go("home");
   }
  
